f7b999dd5e43b1191d3daf973d9627959de69384
QUDUSKUNLE <quduskunle@gmail.com>
2017-05-16 22:09:24 +0100

    Solution to findMinMax function

---
+++ Coding_labs/Coding_labs_2/findMinMax.js
@@ -0,0 +1,16 @@
+findMinMax = (n) => {
+    let min;
+    let max;
+    let result = [];
+    // check for minimum value
+    if (Math.min.apply(null, n) === Math.max.apply(null, n)) {
+        result.push(Math.max.apply(null, n));
+        return result;
+    } else {
+        result.push(Math.min.apply(null, n));
+        result.push(Math.max.apply(null, n));
+        return result;
+    }
+}
+
+module.exports = findMinMax;
